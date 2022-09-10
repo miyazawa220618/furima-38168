2a0140c56cd66a675bb262892d318ed46c52e8a6
miyazawa220618 <m.yukika341@gmail.com>
2022-09-10 22:17:34 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  valid_name = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  valid_kana = /\A[ァ-ヶー－]+\z/
+
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name,       presence: true, format: { with: valid_name }
+  validates :first_name,      presence: true, format: { with: valid_name }
+  validates :last_name_kana,  presence: true, format: { with: valid_kana }
+  validates :first_name_kana, presence: true, format: { with: valid_kana }
   validates :birth_date,      presence: true
 end
