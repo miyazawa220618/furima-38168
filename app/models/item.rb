bcd95ef271c2a688f334086a1fe7374b8ec73919
miyazawa220618 <m.yukika341@gmail.com>
2022-09-11 22:07:11 +0900

    バリデーション実装

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name,                   presence: true
+  validates :info,                   presence: true
+  validates :category_id,            presence: true
+  validates :sales_status_id,        presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :scheduled_delivery_id,  presence: true
+  validates :price,                  presence: true
+end
