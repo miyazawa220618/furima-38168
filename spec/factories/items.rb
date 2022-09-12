7b812319bba2b374e9cbbe31270b9042fa134fa0
miyazawa220618 <m.yukika341@gmail.com>
2022-09-12 21:04:26 +0900

    FactoryBotを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,16 @@ FactoryBot.define do
   factory :item do
     name                   { Faker::Lorem.words }
     info                   { Faker::Lorem.sentence }
-    price                  { Faker::Number.between(300, 9999999) }
-    item_image             { Faker::Lorem.sentence }
-    category_id            { Faker::Number.between(2, 11) }
-    sales_status_id        { Faker::Number.between(2, 7) }
-    shipping_fee_status_id { Faker::Number.between(2, 3) }
-    prefecture_id          { Faker::Number.between(2, 48) }
-    scheduled_delivery_id  { Faker::Number.between(2, 4) }
+    price                  { Faker::Number.between(from: 300, to: 9999999) }
+    category_id            { Faker::Number.between(from: 2, to: 11) }
+    sales_status_id        { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_status_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id          { Faker::Number.between(from: 2, to: 48) }
+    scheduled_delivery_id  { Faker::Number.between(from: 2, to: 4) }
+    association :user
+
+    after(:build) do |item|
+      item.item_image.attach(io: File.open('public/images/sample1.png'), filename: 'sample1.png')
+    end
   end
 end
