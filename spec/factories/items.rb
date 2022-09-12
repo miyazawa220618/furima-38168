954b27ad636a5cee01f4bd447b9d0b5322744961
miyazawa220618 <m.yukika341@gmail.com>
2022-09-12 20:51:04 +0900

    itemモデル単体テストコードの雛形作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   { Faker::Lorem.words }
+    info                   { Faker::Lorem.sentence }
+    price                  { Faker::Number.between(300, 9999999) }
+    item_image             { Faker::Lorem.sentence }
+    category_id            { Faker::Number.between(2, 11) }
+    sales_status_id        { Faker::Number.between(2, 7) }
+    shipping_fee_status_id { Faker::Number.between(2, 3) }
+    prefecture_id          { Faker::Number.between(2, 48) }
+    scheduled_delivery_id  { Faker::Number.between(2, 4) }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品商品の登録' do
+    context '出品商品を登録できる場合' do
+      it '全ての情報が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品商品を登録できない場合' do
+      it 'item_imageが未選択の場合は登録できない' do
+        
+      end
+      it 'nameが空の場合は登録できない' do
+        
+      end
+      it 'infoが空の場合は登録できない' do
+        
+      end
+      it 'category_idが未選択の場合は登録できない' do
+        
+      end
+      it 'sales_status_idが未選択の場合は登録できない' do
+        
+      end
+      it 'shipping_fee_status_idが未選択の場合は登録できない' do
+        
+      end
+      it 'prefecture_idが未選択の場合は登録できない' do
+        
+      end
+      it 'scheduled_delivery_idが未選択の場合は登録できない' do
+        
+      end
+      it 'priceが空の場合は登録できない' do
+        
+      end
+      it 'priceが¥300未満の場合は登録できない' do
+        
+      end
+      it 'priceが¥10,000,000以上の場合は登録できない' do
+        
+      end
+    end
+  end
 end
