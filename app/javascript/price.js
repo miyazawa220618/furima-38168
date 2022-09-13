ccaef523964ab000dc78b03860091f434bced2fb
miyazawa220618 <m.yukika341@gmail.com>
2022-09-13 09:07:36 +0900

    単体テストコードの内容の記述/jsを修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -7,8 +7,22 @@ window.addEventListener('load', function(){
   itemPrice.addEventListener('keyup', () => {
     const priceValue = itemPrice.value;
     taxPrice.innerHTML = Math.round(priceValue * 0.1);
-    
+  
     const taxValue = taxPrice.innerHTML;
     profitPrice.innerHTML = priceValue - taxValue;
-  })
-})
+
+    // 全角入力だった場合
+    if (priceValue.match(/^[^\x01-\x7E\uFF61-\uFF9F]+$/)) {
+      itemPrice.addEventListener('change', () => {
+        const hankakuPrice = priceValue.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+          return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+        });
+        itemPrice.value = hankakuPrice;
+        taxPrice.innerHTML = Math.round(hankakuPrice * 0.1);
+        const hankakuTax = taxPrice.innerHTML;
+        profitPrice.innerHTML = hankakuPrice - hankakuTax;
+      });
+    };
+
+  });
+});
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,37 +13,64 @@ RSpec.describe Item, type: :model do
     end
     context '出品商品を登録できない場合' do
       it 'item_imageが未選択の場合は登録できない' do
-        
+        @item.item_image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item image can't be blank")
       end
       it 'nameが空の場合は登録できない' do
-        
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it 'infoが空の場合は登録できない' do
-        
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
       end
       it 'category_idが未選択の場合は登録できない' do
-        
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'sales_status_idが未選択の場合は登録できない' do
-        
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
       it 'shipping_fee_status_idが未選択の場合は登録できない' do
-        
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
       it 'prefecture_idが未選択の場合は登録できない' do
-        
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'scheduled_delivery_idが未選択の場合は登録できない' do
-        
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
       end
       it 'priceが空の場合は登録できない' do
-        
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが¥300未満の場合は登録できない' do
-        
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it 'priceが¥10,000,000以上の場合は登録できない' do
-        
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'priceが半角数字以外の場合は登録できない' do
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
