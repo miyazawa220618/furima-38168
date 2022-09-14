edc06b05b9fb6c7cac493f44eb57dbc72c08cf13
miyazawa220618 <m.yukika341@gmail.com>
2022-09-14 18:34:21 +0900

    rubocopの自動修正を実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,9 +44,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
   end
 
   def set_item
