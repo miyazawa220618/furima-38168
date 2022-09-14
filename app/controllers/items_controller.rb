4560bdd49b9e78589c6858d342b79c7095ad5534
miyazawa220618 <m.yukika341@gmail.com>
2022-09-14 19:05:32 +0900

    コントローラーの重複箇所を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -43,7 +43,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
   end
 
