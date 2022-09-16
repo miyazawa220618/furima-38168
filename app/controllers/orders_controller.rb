a29d2732cc7ebed5d5a1b7aea2d0beb7c62cf0e2
miyazawa220618 <m.yukika341@gmail.com>
2022-09-16 15:59:57 +0900

    コントローラーの編集とビューの編集
    ・order_adressへ保存する流れに変更
    ・フォームのダミー項目の編集
    ・カード決済を行うcard.jsの作成
    ・PAYJPの鍵情報を環境変数に変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,32 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_address = OrderAddress.new
   end
 
   def create
-    
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
