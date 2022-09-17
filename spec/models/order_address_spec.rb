2dee1dbd68afae2375698f5cc48da3d3346b7523
miyazawa220618 <m.yukika341@gmail.com>
2022-09-17 13:56:57 +0900

    テストコードの内容を記述/ファイル名修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -16,37 +16,56 @@ RSpec.describe Order, type: :model do
         expect(@order_address).to be_valid
       end
       it 'buildingが空でも購入できる' do
-        
+        @order_address.building = ''
+        expect(@order_address).to be_valid
       end
     end
 
     context '商品を購入できない場合' do
       it 'postal_codeが空の場合は購入できない' do
-
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
       end
       it 'postal_codeが「3桁ハイフン4桁」の半角文字列以外の形式の場合は購入できない' do
-        
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
       end
       it 'prefecture_idが未選択の場合は購入できない' do
-        
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'cityが空の場合は購入できない' do
-        
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
       end
       it 'house_numberが空の場合は購入できない' do
-        
+        @order_address.house_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House number can't be blank")
       end
       it 'phone_numberが空の場合は購入できない' do
-        
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_numberが「10桁以上11桁以内の半角数字」以外の場合は購入できない' do
-        
+        @order_address.phone_number = '012345678'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid.")
       end
       it 'userテーブルと紐づいていない場合は購入できない' do
-        
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
       end
       it 'itemテーブルと紐づいていない場合は購入できない' do
-        
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
     end
 
